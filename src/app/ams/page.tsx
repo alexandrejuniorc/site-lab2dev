bed12c90c65216e0c5a8a4e5f7bf2d27854de612
Alexandre Junior <alexandrejunior408@gmail.com>
2023-04-14 17:31:42 -0300

    feat: pages creation

---
+++ src/app/ams/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { BuildingPage } from "@/components/BuildingPage";
+
+export default function AMS() {
+  return <BuildingPage />;
+}
---
+++ src/app/blog/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { BuildingPage } from "@/components/BuildingPage";
+
+export default function Blog() {
+  return <BuildingPage />;
+}
