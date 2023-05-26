7fc30f4290d20aea706b4f584f5555fb1d8ad788
Alexandre Junior <alexandrejunior408@gmail.com>
2023-05-26 12:07:39 -0300

    feat: add favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,6 +14,8 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Varela+Round&display=swap"
           rel="stylesheet"
         />
+
+        <link rel="icon" href="/favicon.ico" />
       </Head>
       <body>
         <Main />
