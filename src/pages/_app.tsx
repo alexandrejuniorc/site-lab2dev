77b59727cf857ed15fc545bf5e16339ae45a25d0
Alexandre Junior <alexandrejunior408@gmail.com>
2023-05-30 14:15:12 -0300

    feat: add title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
-import type { AppProps } from "next/app";
-
 import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import Layout from "@/templates/Layout";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>Lab2dev</title>
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,7 @@ export default function Document() {
           rel="stylesheet"
         />
 
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="./favicon.ico" />
       </Head>
       <body>
         <Main />
