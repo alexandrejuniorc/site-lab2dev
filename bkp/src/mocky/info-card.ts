91f104dfa01ecaabe887e501e90607eea205b60a
Alexandre Junior <alexandrejunior408@gmail.com>
2023-06-06 11:24:42 -0300

    refactor: remove backup from archives

--- bkp/src/mocky/info-card.ts
+++
@@ -1,9 +0,0 @@
-export const infoCard = [
-  { title: "PRESENÇA", info: "+40", content: "clientes atendidos" },
-  { title: "DEDICAÇÃO", info: "+140", content: "colaboradores" },
-  {
-    title: "EXPERIÊNCIA",
-    info: "+750k horas",
-    content: "em projetos de inovação",
-  },
-];
--- bkp/src/pages/404.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import { PageNotFound } from "@/components/NotFound";
-
-export default function notFound() {
-  return <PageNotFound />;
-}
